4a7efa9b3b7546a7e84544c751cc88c2c2d42cc8
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-21 02:00:16 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express()
@@ -8,9 +9,32 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9ragi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect()
+        const servicesCollection = client.db('dental_cares').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query);
+            const services =await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+
+
+
+
+
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log('listening from', port)
